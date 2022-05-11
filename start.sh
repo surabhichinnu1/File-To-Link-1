2448f75090e0614bcbea8c71e38d461ea7821bac
MKN TG <86054899+MrMKN@users.noreply.github.com>
2022-05-11 22:11:59 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/MrMKN/File-To-Link.git /File-To-Link     
+  git clone https://github.com/surabhichinnu1/File-To-Link-1.git /File-To-Link     
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /File-To-Link
